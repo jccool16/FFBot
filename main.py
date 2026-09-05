15a792e2a41be849e5c5ee7401196448eb41d2f0
Jake Franke <113054625+jccool16@users.noreply.github.com>
2026-09-05 08:41:12 -0700

    Final fix weekly performers

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ for team in league.teams:
     for player in team.roster:
         player_stats.append({
             'name': player.name,
-            'points': player.stats[week_num].get('points', 0),
+            'points': player.stats[week_num - 1]['points'],
             'team_name': team.team_name
         })
 
